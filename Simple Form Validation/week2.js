fc907cd63e4c1d08bf8db533e4eef403d9b2125e
008022228 <008022228@N203D5-23>
2024-08-21 14:45:25 -0400

    AHHHHHH

--- Simple Form Validation/week2.js
+++ Simple Form Validation/week2.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+var input = document.querySelector(`input #first-name`)
+var text = document.querySelector(`span #fn-error`)
+var btn = document.querySelector(`input[type="button"]`)
+
+btn.addEventListener(`click`, e=>{
+    console.log(`HELLO`)
+})
+
+
+
