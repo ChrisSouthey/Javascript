66566b84a4a423d81a28f62230d210fdefed4eaa
008022228 <008022228@N203D5-23>
2024-08-28 11:39:52 -0400

    feufoueqf

--- Simple Form Validation/week2.js
+++ Simple Form Validation/week2.js
@@ -4,18 +4,23 @@ var input = Array.from(document.querySelectorAll(`form input[type="text"]`))
 var p = Array.from(document.querySelectorAll(`form p`))
 var text = Array.from(document.querySelectorAll(`span`))
 var btn = document.querySelector(`input[type="button"]`)
-var rxName = /^[a-z ,.'-]+$/i
+var rxName = /^[a-zA-Z-]+$/
 
 btn.addEventListener(`click`, e=>{
     for (var i=0;i<input.length;i++){
-        if(input[i].value == ""){
+        /*if(input[i].value == ""){
             text[i].innerHTML = `*`
             text[i].style.color = `red`
             p[i].style.color = `red`
+        }*/
+        if(rxName.test(input[0]) && input[0].value != ""){
+            text[i].innerHTML = ``
+            text[i].style.color = `black`
+            p[i].style.color = `black`
         }
         else{
-            text[i].innerHTML = ``
-            p[i].style.color = `black`
+            text[i].innerHTML = `*`
+            p[i].style.color = `red`
         }
 
     }
