6770018b26e14704241714e53ec8d5af3ac2595f
ChrisSouthey <chsouthey@email.neit.edu>
2024-09-18 13:24:26 -0400

    DONE

--- form/week2.js
+++ form/week2.js
@@ -9,7 +9,7 @@ var form = document.querySelector(`form`);
 var div = document.querySelector(`#confirmation`);
 var pCon = document.querySelector(`p#info`);
 
-var rxFName = /^[a-zA-Z-]+$/
+var rxFName = /^[a-zA-Z]+$/
 var rxLName = /^[a-zA-Z-]+$/
 var rxEmail = /(?:[a-z0-9!#$%&'[+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'+/=?^_{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 var rxPhone = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
